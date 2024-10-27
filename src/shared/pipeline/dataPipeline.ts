31ad44b6640b9e796a306e9a796d814f8ce83e15
Martin Dallinger <martindallinger2002@gmail.com>
2024-10-27 12:55:42 +0100

    standardized y

--- src/shared/pipeline/dataPipeline.ts
+++ src/shared/pipeline/dataPipeline.ts
@@ -10,6 +10,22 @@ export function executeDataPipeline(
 ): { records: Record[]; numericalKeys: string[]; categoricalKeys: string[] } {
     let records: Record[] = parseCSV(data, metadata.split_char);
 
+    // standardize the target variable
+    let target_mean = 0;
+    let target_std = 1;
+    const targetValues = records.map(record => parseFloat(record[metadata.target_var] as string));
+    target_mean = targetValues.reduce((a, b) => a + b, 0) / targetValues.length;
+    target_std = Math.sqrt(
+        targetValues.reduce((a, b) => a + (b - target_mean) ** 2, 0) / targetValues.length
+    );
+    records = records.map(record => {
+        record[metadata.target_var] = (
+            (parseFloat(record[metadata.target_var] as string) - target_mean) /
+            target_std
+        ).toString();
+        return record;
+    });
+
     let numericalKeys: string[] = Object.keys(records[0]).filter(key => {
         return records.every(record => !isNaN(parseFloat(record[key] as string)));
     });
